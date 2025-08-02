77b47af8f1e7969d082a22e6c7bb1cf09de35f33
danilov6083 <danilov6083@outlook.com>
2025-08-02 03:44:17 +0300

    DPKVS-2: refactored logger (de- and serializing was separated into new class)

---
+++ dpkvs/append_only_log/log_serializer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+#include <dpkvs/engine/storable_value.h>
+#include <dpkvs/append_only_log/operations.h>
+
+namespace NKVStore::NAppendLog
+{
+
+    class TAppendLogSerializer
+    {
+    public:
+        TAppendLogSerializer();
+
+        explicit TAppendLogSerializer(std::string& fileName);
+
+        TAppendLogSerializer(TAppendLogSerializer&) = delete;
+        TAppendLogSerializer& operator=(const TAppendLogSerializer&) = delete;
+
+        TAppendLogSerializer(TAppendLogSerializer&&) noexcept;
+        TAppendLogSerializer& operator=(TAppendLogSerializer&&) noexcept;
+
+        ~TAppendLogSerializer();
+
+        bool ReadyToRead();
+
+        void WritePutLog(
+            const std::string& key,
+            const NEngine::TStorableValue& value);
+
+        void WriteRemoveLog(const std::string& key);
+
+        EAppendLogOperations ReadCommand();
+        std::string ReadKey();
+        NEngine::TStorableValue ReadValue();
+
+    private:
+        template <class T>
+        T ReadBinary();
+
+        void Flush();
+
+        void OpenFileStream();
+
+        std::fstream _log_stream;
+
+        std::string _fileName = "append-only-log.txt";
+    };
+
+} // namespace NKVStore::NAppendLog
